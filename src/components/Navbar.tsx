2c6c69b2bdbb7f47fd130f6e832c59cba3fb160b
arnu515 <52203828+arnu515@users.noreply.github.com>
2025-06-02 13:48:49 +0530

    feat: show dashboard button if logged in

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,40 +1,45 @@
+import { component$ } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
+import { useUser } from "~/lib/user";
 
-export default () => (
-  <nav class="relative px-4 py-6">
-    <div class="container mx-auto flex items-center justify-between">
-      <div class="flex items-center space-x-2">
-        <img src="/favicon.svg" alt="Logo" class="h-8 w-8" />
-        <Link href="/" class="text-xl font-bold text-white">
-          SupportAddress
-        </Link>
+export default component$(() => {
+  const user = useUser();
+  return (
+    <nav class="relative px-4 py-6">
+      <div class="container mx-auto flex items-center justify-between">
+        <div class="flex items-center space-x-2">
+          <img src="/favicon.svg" alt="Logo" class="h-8 w-8" />
+          <Link href="/" class="text-xl font-bold text-white">
+            SupportAddress
+          </Link>
+        </div>
+        <div class="hidden items-center space-x-8 md:flex">
+          <a
+            href="/#features"
+            class="text-gray-300 transition-colors hover:text-white"
+          >
+            Features
+          </a>
+          <Link
+            href="/pricing"
+            class="text-gray-300 transition-colors hover:text-white"
+          >
+            Pricing
+          </Link>
+          <Link
+            href="/contact"
+            class="text-gray-300 transition-colors hover:text-white"
+          >
+            Contact
+          </Link>
+          <Link
+            href={user ? "/app" : "/auth"}
+            class="cursor-pointer rounded-lg border border-purple-400 bg-white px-4 py-2 text-purple-500 transition-colors duration-300 hover:bg-purple-400 hover:text-white"
+          >
+            {user ? "Dashboard" : "Sign In"}
+          </Link>
+        </div>
       </div>
-      <div class="hidden items-center space-x-8 md:flex">
-        <a
-          href="/#features"
-          class="text-gray-300 transition-colors hover:text-white"
-        >
-          Features
-        </a>
-        <Link
-          href="/pricing"
-          class="text-gray-300 transition-colors hover:text-white"
-        >
-          Pricing
-        </Link>
-        <Link
-          href="/contact"
-          class="text-gray-300 transition-colors hover:text-white"
-        >
-          Contact
-        </Link>
-        <Link
-          href="/auth"
-          class="cursor-pointer rounded-lg border border-purple-400 bg-white px-4 py-2 text-purple-500 transition-colors duration-300 hover:bg-purple-400 hover:text-white"
-        >
-          Sign In
-        </Link>
-      </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+});
