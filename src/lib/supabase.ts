1757f2d8a6ecb1dd7304268dbcf881f687802fad
arnu515 <52203828+arnu515@users.noreply.github.com>
2025-06-02 13:46:20 +0530

    feat: change function sig

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,15 +1,24 @@
 import type {RequestEventBase} from "@builder.io/qwik-city"
 import { createServerClient } from "@supabase/ssr"
+import type {Database} from "./dbTypes"
+import { createClient } from "@supabase/supabase-js"
 
-export function createSupabaseServerClient(req: RequestEventBase, {serviceRole=false}: {serviceRole?: boolean} = {}) {
-  return createServerClient(
-    req.env.get("PUBLIC_SUPABASE_URL")!,
-    serviceRole ? req.env.get("SUPABASE_SERVICE_ROLE_KEY")! : req.env.get("PUBLIC_SUPABASE_ANON_KEY")!,
+export function createSupabaseServerClient({env, cookie}: {env: RequestEventBase["env"], cookie: RequestEventBase["cookie"]}, {serviceRole=false}: {serviceRole?: boolean} = {}) {
+  return createServerClient<Database>(
+    env.get("PUBLIC_SUPABASE_URL")!,
+    serviceRole ? env.get("SUPABASE_SERVICE_ROLE_KEY")! : env.get("PUBLIC_SUPABASE_ANON_KEY")!,
     {
       cookies: {
-        getAll: () => Object.entries(req.cookie.getAll()).map(([k, {value}]) => ({ name: k, value })),
-        setAll: (cookies) => cookies.forEach(({name, value, options}) => req.cookie.set(name, value, options))
+        getAll: () => Object.entries(cookie.getAll()).map(([k, {value}]) => ({ name: k, value })),
+        setAll: (cookies) => cookies.forEach(({name, value, options}) => cookie.set(name, value, options))
       }
     }
+  )
+}
+
+export function createServiceRoleClient(env: RequestEventBase["env"]) {
+  return createClient<Database>(
+    env.get("PUBLIC_SUPABASE_URL")!,
+    env.get("SUPABASE_SERVICE_ROLE_KEY")!,
   )
 }
