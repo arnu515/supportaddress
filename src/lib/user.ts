34fbb6a2634fc50cb2d59f80e8ea42453a0142b6
arnu515 <52203828+arnu515@users.noreply.github.com>
2025-06-04 12:00:54 +0530

    feat: fetch user profile

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -1,5 +1,8 @@
 import { routeLoader$ } from "@builder.io/qwik-city";
-import type { User } from "@supabase/supabase-js";
+import type { User as SupabaseUser } from "@supabase/supabase-js";
+import { Database } from "./dbTypes";
+
+export type User = SupabaseUser & { profile: Database["public"]["Tables"]["users"]["Row"] }
 
 export const useUser = routeLoader$((req) => {
   return req.sharedMap.get('user') as User | null;
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -2,18 +2,26 @@ import { component$, Slot } from "@builder.io/qwik";
 import type { RequestHandler } from "@builder.io/qwik-city";
 import { createSupabaseServerClient } from "~/lib/supabase";
 
-export { useUser } from "~/lib/user";
+export { useUser, type User } from "~/lib/user";
 
 export const onRequest: RequestHandler = async (req) => {
   const supabase = createSupabaseServerClient(req);
   const { data, error } = await supabase.auth.getSession();
   if (error) throw req.error(500, error.message);
+  req.sharedMap.set("session", data.session);
   if (data.session) {
     const { data, error } = await supabase.auth.getUser();
     if (error) throw req.error(500, error.message);
-    req.sharedMap.set("user", data.user);
-  } else req.sharedMap.set("user", null);
-  req.sharedMap.set("session", data.session);
+    if (data.user) {
+      const { data: profile, error } = await supabase.from("users").select("*").eq("id", data.user.id).maybeSingle()
+      if (error) throw req.error(500, error.message);
+      if (profile) {
+        req.sharedMap.set("user", { ...data.user, profile } satisfies User);
+        return;
+      }
+    }
+  }
+  req.sharedMap.set("user", null);
 };
 
 export default component$(() => {
