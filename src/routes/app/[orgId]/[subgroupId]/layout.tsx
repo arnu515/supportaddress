c5369095efd784f1fb275f574d9a798facb8b8fe
arnu515 <52203828+arnu515@users.noreply.github.com>
2025-06-08 17:00:32 +0530

    fix: 404 if no subgroup

--- src/routes/app/[orgId]/[subgroupId]/layout.tsx
+++ src/routes/app/[orgId]/[subgroupId]/layout.tsx
@@ -11,6 +11,7 @@ export const useSubgroup = routeLoader$(async (req) => {
     .eq("id", subgroupId)
     .maybeSingle();
   if (error) throw req.error(500, error.message);
+  if (!data) throw req.error(404, "Page not found")
   return data;
 });
 
