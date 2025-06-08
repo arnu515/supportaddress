357b917913f87748c466e4ef7b99bf90a6c41b8d
arnu515 <52203828+arnu515@users.noreply.github.com>
2025-06-09 01:02:01 +0530

    feat: add editor component

---
+++ src/components/editor.tsx
@@ -0,0 +1,120 @@
+import {
+  type QRL,
+  component$,
+  useSignal,
+  useVisibleTask$,
+} from "@builder.io/qwik";
+
+export type EditorProps = {
+  onContentChange$?: QRL<(content: string) => void>;
+};
+
+export default component$(({ onContentChange$ }: EditorProps) => {
+  const editorRef = useSignal<HTMLDivElement>();
+
+  useVisibleTask$(async () => {
+    const { init, exec } = await import("pell");
+    if (editorRef.value) {
+      init({
+        element: editorRef.value,
+        onChange: (html) => onContentChange$?.(html),
+        defaultParagraphSeparator: "div",
+        styleWithCSS: false,
+        actions: [
+          {
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-4"><path d="M6 12h9a4 4 0 0 1 0 8H7a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h7a4 4 0 0 1 0 8"/></svg>',
+            title: "Bold",
+            result: () => exec("bold"),
+          },
+          {
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-4"><line x1="19" x2="10" y1="4" y2="4"/><line x1="14" x2="5" y1="20" y2="20"/><line x1="15" x2="9" y1="4" y2="20"/></svg>',
+            title: "Italic",
+            result: () => exec("italic"),
+          },
+          {
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-4"><path d="M6 4v6a6 6 0 0 0 12 0V4"/><line x1="4" x2="20" y1="20" y2="20"/></svg>',
+            title: "Underline",
+            result: () => exec("underline"),
+          },
+          {
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-4"><path d="M16 4H9a3 3 0 0 0-2.83 4"/><path d="M14 12a4 4 0 0 1 0 8H6"/><line x1="4" x2="20" y1="12" y2="12"/></svg>',
+            title: "Strike-through",
+            result: () => exec("strikeThrough"),
+          },
+          {
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-4"><path d="M4 12h8"/><path d="M4 18V6"/><path d="M12 18V6"/><path d="m17 12 3-2v8"/></svg>',
+            title: "Heading 1",
+            result: () => exec("formatBlock", "<h1>"),
+          },
+          {
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-4"><path d="M4 12h8"/><path d="M4 18V6"/><path d="M12 18V6"/><path d="M21 18h-4c0-4 4-3 4-6 0-1.5-2-2.5-4-1"/></svg>',
+            title: "Heading 2",
+            result: () => exec("formatBlock", "<h2>"),
+          },
+          {
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-4"><path d="M4 12h8"/><path d="M4 18V6"/><path d="M12 18V6"/><path d="M17.5 10.5c1.7-1 3.5 0 3.5 1.5a2 2 0 0 1-2 2"/><path d="M17 17.5c2 1.5 4 .3 4-1.5a2 2 0 0 0-2-2"/></svg>',
+            title: "Heading 3",
+            result: () => exec("formatBlock", "<h3>"),
+          },
+          {
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-4"><path d="M13 4v16"/><path d="M17 4v16"/><path d="M19 4H9.5a4.5 4.5 0 0 0 0 9H13"/></svg>',
+            title: "Paragraph",
+            result: () => exec("formatBlock", "<p>"),
+          },
+          {
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-4"><path d="M16 3a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2 1 1 0 0 1 1 1v1a2 2 0 0 1-2 2 1 1 0 0 0-1 1v2a1 1 0 0 0 1 1 6 6 0 0 0 6-6V5a2 2 0 0 0-2-2z"/><path d="M5 3a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2 1 1 0 0 1 1 1v1a2 2 0 0 1-2 2 1 1 0 0 0-1 1v2a1 1 0 0 0 1 1 6 6 0 0 0 6-6V5a2 2 0 0 0-2-2z"/></svg>',
+            title: "Quote",
+            result: () => exec("formatBlock", "<blockquote>"),
+          },
+          {
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-4"><path d="M10 12h11"/><path d="M10 18h11"/><path d="M10 6h11"/><path d="M4 10h2"/><path d="M4 6h1v4"/><path d="M6 18H4c0-1 2-2 2-3s-1-1.5-2-1"/></svg>',
+            title: "Ordered List",
+            result: () => exec("insertOrderedList"),
+          },
+          {
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-4"><path d="M3 12h.01"/><path d="M3 18h.01"/><path d="M3 6h.01"/><path d="M8 12h13"/><path d="M8 18h13"/><path d="M8 6h13"/></svg>',
+            title: "Unordered List",
+            result: () => exec("insertUnorderedList"),
+          },
+          {
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-4"><path d="m16 18 6-6-6-6"/><path d="m8 6-6 6 6 6"/></svg>',
+            title: "Code",
+            result: () => exec("formatBlock", "<pre>"),
+          },
+          {
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-4"><path d="M5 12h14"/></svg>',
+            title: "Horizontal Line",
+            result: () => exec("insertHorizontalRule"),
+          },
+          {
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-4"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"/><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"/></svg>',
+            title: "Link",
+            result: () => {
+              const url = window.prompt("Enter the link URL");
+              if (url) exec("createLink", url);
+            },
+          },
+          {
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-4"><rect width="18" height="18" x="3" y="3" rx="2" ry="2"/><circle cx="9" cy="9" r="2"/><path d="m21 15-3.086-3.086a2 2 0 0 0-2.828 0L6 21"/></svg>',
+            title: "Image Link",
+            result: () => {
+              const url = window.prompt("Enter the image URL");
+              if (url) exec("insertImage", url);
+            },
+          },
+        ],
+        classes: {
+          actionbar:
+            "flex items-center flex-wrap px-2 py-1 border border-purple-400/30 rounded-tr-sm rounded-tl-sm gap-2",
+          button:
+            "p-1 cursor-pointer w-8 h-8 border border-gray-300 dark:border-gray-700 flex items-center justify-center",
+          selected: "bg-purple-100 dark:bg-purple-900/30 border-purple-500",
+          content:
+            "px-4 py-2 border border-purple-400/30 -mt-[1px] focus:outline-none focus:border-purple-400",
+        },
+      });
+    }
+  });
+
+  return <div ref={editorRef} />;
+});
