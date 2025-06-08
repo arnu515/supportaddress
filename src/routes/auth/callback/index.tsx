f396d05f2ad68e418d744dc2d06894c676bda645
arnu515 <52203828+arnu515@users.noreply.github.com>
2025-06-09 02:49:34 +0530

    feat: show auth error if any

--- src/routes/auth/callback/index.tsx
+++ src/routes/auth/callback/index.tsx
@@ -9,5 +9,10 @@ export const onGet: RequestHandler = async (req) => {
     if (!error) throw req.redirect(303, "/app");
     throw req.error(500, error.message);
   }
+  const error_code = req.url.searchParams.get("error_code");
+  if (error_code) {
+    const error_description = req.url.searchParams.get("error_description");
+    throw req.error(400, `${error_code}: ${error_description}`);
+  }
   throw req.redirect(303, "/auth");
 };
