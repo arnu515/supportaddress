a7e882107852d3ade9a19545cad5ff0661e4aefa
arnu515 <52203828+arnu515@users.noreply.github.com>
2025-06-09 05:45:39 +0530

    fix: make node:buffer an external dep

--- vite.config.ts
+++ vite.config.ts
@@ -31,9 +31,9 @@ export default defineConfig(({ command, mode }): UserConfig => {
     /**
      * This is an advanced setting. It improves the bundling of your server code. To use it, make sure you understand when your consumed packages are dependencies or dev dependencies. (otherwise things will break in production)
      */
-    // ssr:
-    //   command === "build" && mode === "production"
-    //     ? {
+    ssr:
+      command === "build" && mode === "production"
+        ? {
     //         // All dev dependencies should be bundled in the server build
     //         noExternal: Object.keys(devDependencies),
     //         // Anything marked as a dependency will not be bundled
@@ -41,9 +41,9 @@ export default defineConfig(({ command, mode }): UserConfig => {
     //         // If a dep-of-dep needs to be external, add it here
     //         // For example, if something uses `bcrypt` but you don't have it as a dep, you can write
     //         // external: [...Object.keys(dependencies), 'bcrypt']
-    //         external: Object.keys(dependencies),
-    //       }
-    //     : undefined,
+            external: ['node:buffer'],
+          }
+        : undefined,
     server: {
       headers: {
         // Don't cache the server response in dev mode
